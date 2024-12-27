499a2aaedacc03fd29b268462541a52118dbbe63
唯二 <vear.vip@qq.com>
2024-12-27 07:19:54 +0000

    更新腳本

--- src/compare.ts
+++ src/compare.ts
@@ -1,19 +1,51 @@
 
 import fs from 'fs'
 import path from 'path'
-import { extractHanzi } from '@vearvip/hanzi-utils'
+import { extractHanzi } from '@vearvip/hanzi-utils' 
 
-const referCharList = []
-// 镇头字表、田坪字表路径
-const tsvPath = path.resolve(__dirname, `../${process.argv[2]}`) 
-const originTsvText = fs.readFileSync(tsvPath, 'utf8')
-// 提取汉字
-const originCharList = [... new Set([...extractHanzi(originTsvText.replace(/$.*?$/g, ''))])]
-console.log('originCharList', originCharList)
+const referList = [
+  '訓詁諧音.tsv'
+]
 
-const resultList: string[] = []
-referCharList.forEach(char => {
-  if (!originCharList.includes(char)) {
-    resultList.push(char)
-  }
-})
+function getCharList(tsvPath: string): [string, string[]] {
+  const tsvText = fs.readFileSync(tsvPath, 'utf8')
+  // 提取汉字
+  const charList: string[] = [... new Set([...extractHanzi(tsvText.replace(/$.*?$/g, ''))])]
+  return [tsvText, charList]  
+}
+
+// 镇头字表、田坪字表
+const [,originCharList] = getCharList(path.resolve(__dirname, `../${process.argv[2]}`))
+// 對比字表
+const [referStr,referCharList] = getCharList(path.resolve(__dirname, `../refer/${referList[0]}`))
+// console.log({
+//   originCharList,
+//   referCharList
+// })
+
+try {
+  const [omissionList, includeList]: string[][] = referCharList.reduce((ret: string[][], char: string) => {
+    if (!Array.isArray(ret[0])) {
+      ret[0] = []
+    }
+    if (!Array.isArray(ret[1])) {
+      ret[1] = []
+    }
+    if (!originCharList.includes(char)) {
+      ret[0].push(char)
+    } else {
+      ret[1].push(char)
+    }
+    return ret
+  }, [])
+  console.log(`✅ 對比結束，漏了${omissionList.length}個字：`, omissionList)
+  let resultStr = referStr
+  includeList.forEach(char => {
+    resultStr = resultStr.replaceAll(char, '')
+  })
+  fs.writeFileSync(path.resolve(__dirname, '../omission/char.tsv'), resultStr)
+} catch (error) {
+  console.error(`⚠️ 對比失敗：`, error)
+}
+
+ 
