bfbb722c4a9c6eb5fefd3e1ba1d964f6a8f6f99a
唯二 <vear.vip@qq.com>
2025-03-06 02:39:48 +0000

    尝试通过加入音韵地位转换

---
+++ src/try/index.ts
@@ -0,0 +1,9 @@
+import TshetUinh from "tshet-uinh";
+
+const reqRet1 = TshetUinh.資料.query字頭("結");
+const reqRet2 = TshetUinh.資料.query字頭("书");
+const reqRet3 = TshetUinh.資料.query字頭("二");
+
+console.log("1：", reqRet1);
+console.log("2：", reqRet2);
+console.log("3：", reqRet3);
