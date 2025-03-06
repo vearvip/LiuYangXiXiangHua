73e1af8d276c6bcc713e7dd253ebea5ccc8c353d
唯二 <vear.vip@qq.com>
2025-03-06 04:44:02 +0000

    编写浏阳官桥转换规则

--- src/transform/convert.ts
+++ src/transform/convert.ts
@@ -1,63 +1,143 @@
-import { CharList, saveTsv, ShengDiao, YinBiao } from "./util"
-
+import { parseString, parseStringObject, removeLetters } from "../utils";
+import { CharList, saveTsv, ShengDiao, ShiYi, YinBiao } from "./util";
+import TshetUinh from "tshet-uinh";
+import fs from "fs";
 
 // 生成浏阳镇头.tsv
 export function genZhenTou(data: CharList) {
-  const charList: CharList = JSON.parse(JSON.stringify(data)) 
-  saveTsv(charList, `浏阳镇头.tsv`)
+  const charList: CharList = JSON.parse(JSON.stringify(data));
+  saveTsv(charList, `浏阳镇头.tsv`);
 }
 // 生成浏阳田坪.tsv
 export function genTianPing(data: CharList) {
-  const charList: CharList = JSON.parse(JSON.stringify(data))
-  charList.forEach(ele => {
+  const charList: CharList = JSON.parse(JSON.stringify(data));
+  charList.forEach((ele) => {
     if (
-      (Number(ele[ShengDiao]) === 2 || Number(ele[ShengDiao]) === 6) && ele[YinBiao].includes('ʰ')
+      (Number(ele[ShengDiao]) === 2 || Number(ele[ShengDiao]) === 6) &&
+      ele[YinBiao].includes("ʰ")
     ) {
-      ele[YinBiao] = ele[YinBiao].replace('ʰ', '')
+      ele[YinBiao] = ele[YinBiao].replace("ʰ", "");
     }
-  })
-  saveTsv(charList, `浏阳田坪.tsv`)
+  });
+  saveTsv(charList, `浏阳田坪.tsv`);
 }
 
 // 生成浏阳官桥.tsv
 export function genGuanQiao(data: CharList) {
-  const charList: CharList = JSON.parse(JSON.stringify(data))
-  charList.forEach(ele => {
-    if ( // 混淆c 和 ts 为 tɕ
-      (ele[YinBiao].includes('c') || ele[YinBiao].includes('ts'))
-      && (ele[YinBiao].includes('i') || ele[YinBiao].includes('y'))
+  const charList: CharList = JSON.parse(JSON.stringify(data));
+  let eiList: parseStringObject = [],
+    ouList: parseStringObject = [];
+  charList.forEach((ele) => {
+    if (
+      // 混淆c 和 ts 为 tɕ
+      (ele[YinBiao].includes("c") || ele[YinBiao].includes("ts")) &&
+      (ele[YinBiao].includes("i") || ele[YinBiao].includes("y"))
     ) {
-      ele[YinBiao] = ele[YinBiao].replace('c', 'tɕ')
-      ele[YinBiao] = ele[YinBiao].replace('ts', 'tɕ')
+      ele[YinBiao] = ele[YinBiao].replace("c", "tɕ");
+      ele[YinBiao] = ele[YinBiao].replace("ts", "tɕ");
     }
-    if ( // 混淆cʰ 和 tsʰ 为 tɕʰ
-      (ele[YinBiao].includes('cʰ') || ele[YinBiao].includes('tsʰ'))
-      && (ele[YinBiao].includes('i') || ele[YinBiao].includes('y'))
+    if (
+      // 混淆cʰ 和 tsʰ 为 tɕʰ
+      (ele[YinBiao].includes("cʰ") || ele[YinBiao].includes("tsʰ")) &&
+      (ele[YinBiao].includes("i") || ele[YinBiao].includes("y"))
     ) {
-      ele[YinBiao] = ele[YinBiao].replace('cʰ', 'tɕʰ')
-      ele[YinBiao] = ele[YinBiao].replace('tsʰ', 'tɕʰ')
+      ele[YinBiao] = ele[YinBiao].replace("cʰ", "tɕʰ");
+      ele[YinBiao] = ele[YinBiao].replace("tsʰ", "tɕʰ");
     }
-    if ( // 混淆ç 和 s 为 ɕ
-      (ele[YinBiao].includes('ç') || ele[YinBiao].includes('s'))
-      && (ele[YinBiao].includes('i') || ele[YinBiao].includes('y'))
+    if (
+      // 混淆ç 和 s 为 ɕ
+      (ele[YinBiao].includes("ç") || ele[YinBiao].includes("s")) &&
+      (ele[YinBiao].includes("i") || ele[YinBiao].includes("y"))
     ) {
-      ele[YinBiao] = ele[YinBiao].replace('ç', 'ɕ')
-      ele[YinBiao] = ele[YinBiao].replace('s', 'ɕ')
+      ele[YinBiao] = ele[YinBiao].replace("ç", "ɕ");
+      ele[YinBiao] = ele[YinBiao].replace("s", "ɕ");
     }
 
     // 转换ɲ 为 ȵ
-    ele[YinBiao] = ele[YinBiao].replace('ɲ', 'ȵ')
-    // 转换əu 为 ei
-    if (  
-      ele[YinBiao].includes('əu')
-      && !ele[YinBiao].includes('i')
-    ) {
-      ele[YinBiao] = ele[YinBiao].replace('əu', 'ei')
-    }
+    ele[YinBiao] = ele[YinBiao].replace("ɲ", "ȵ");
     // 转换oŋ 为 əŋ
-    ele[YinBiao] = ele[YinBiao].replace('oŋ', 'əŋ')
+    ele[YinBiao] = ele[YinBiao].replace("oŋ", "əŋ");
 
+    // 转换əu 为 ei
+    if (ele[YinBiao].includes("əu") && !ele[YinBiao].includes("i")) {
+      const charArr = parseString(ele[ShiYi]);
 
-  })
-  saveTsv(charList, `浏阳官桥.tsv`)
+      charArr.forEach((item) => {
+        const queryInfoList = TshetUinh.資料.query字頭(item.zi);
+        queryInfoList.forEach((queryInfo) => {
+          // console.log('queryInfo["音韻地位"]', {
+          //   母: queryInfo["音韻地位"].母,
+          //   呼: queryInfo["音韻地位"].呼,
+          //   等: queryInfo["音韻地位"].等,
+          //   類: queryInfo["音韻地位"].類,
+          //   韻: queryInfo["音韻地位"].韻,
+          //   聲: queryInfo["音韻地位"].聲,
+          // });
+
+          if (
+            (queryInfo["音韻地位"].等 === "三" &&
+              queryInfo["音韻地位"].韻 === "尤") ||
+            (queryInfo["音韻地位"].等 === "一" &&
+              queryInfo["音韻地位"].韻 === "侯")
+          ) {
+            eiList.push({
+              ...item,
+              [ShengDiao]: ele[ShengDiao],
+              [YinBiao]: ele[YinBiao],
+            });
+          } else {
+            ouList.push({
+              ...item,
+              [ShengDiao]: ele[ShengDiao],
+              [YinBiao]: ele[YinBiao],
+            });
+          }
+        });
+      });
+    }
+  });
+
+  charList.forEach((ele) => {
+    // 回填əu
+    if (ele[YinBiao].includes("əu") && !ele[YinBiao].includes("i")) {
+      ele[ShiYi] = ouList
+        .filter((ouItem) => {
+          return (
+            ouItem[YinBiao] == ele[YinBiao] &&
+            ouItem[ShengDiao] == ele[ShengDiao]
+          );
+        })
+        .map((ouItem) => {
+          return (
+            ouItem.zi +
+            (ouItem.baiDu ? "-" : "") +
+            (ouItem.wenDu ? "=" : "") +
+            ouItem.shiYi
+          );
+        })
+        .join("");
+    }
+    // 回填əu
+    if (ele[YinBiao].includes("ei")) {
+      ele[ShiYi] += eiList
+        .filter((eiItem) => {
+          return (
+            eiItem[YinBiao].replace("əu", "ei") == ele[YinBiao] &&
+            eiItem[ShengDiao] == ele[ShengDiao]
+          );
+        })
+        .map((eiItem) => {
+          return (
+            eiItem.zi +
+            (eiItem.baiDu ? "-" : "") +
+            (eiItem.wenDu ? "=" : "") +
+            eiItem.shiYi
+          );
+        })
+        .join("");
+    }
+  });
+  saveTsv(charList, `浏阳官桥.tsv`);
+  saveTsv(eiList, `浏阳官桥2.tsv`);
+  // fs.writeFile("./xx.json", JSON.stringify(eiList), (err) => {});
 }
--- src/transform/util.ts
+++ src/transform/util.ts
@@ -1,17 +1,16 @@
+import fs from "fs";
+import path from "path";
+import csvParser from "csv-parser";
 
-import fs from 'fs';
-import path from 'path';
-import csvParser from 'csv-parser';
-
-export const YinBiao = 'YinBiao'
-export const ShengDiao = 'ShengDiao'
-export const ShiYi = 'ShiYi'
+export const YinBiao = "YinBiao";
+export const ShengDiao = "ShengDiao";
+export const ShiYi = "ShiYi";
 
 export type CharList = Array<{
   [YinBiao]: string;
   [ShengDiao]: string;
   [ShiYi]: string;
-}>
+}>;
 
 // 手动定义表头
 export const headers = [YinBiao, ShengDiao, ShiYi]; // 根据你的实际情况修改
@@ -22,40 +21,43 @@ export function readTsv(filePath: string): Promise<CharList> {
     const dataArray: CharList = [];
 
     stream
-      .pipe(csvParser({
-        separator: '\t',
-        headers: headers, // 手动指定表头，不从文件读取
-        skipLines: 0 // 确保不跳过任何行
-      }))
-      .on('data', (row) => {
+      .pipe(
+        csvParser({
+          separator: "\t",
+          headers: headers, // 手动指定表头，不从文件读取
+          skipLines: 0, // 确保不跳过任何行
+        })
+      )
+      .on("data", (row) => {
         dataArray.push(row);
       })
-      .on('end', () => {
+      .on("end", () => {
         resolve(dataArray);
       })
-      .on('error', (error) => {
+      .on("error", (error) => {
         reject(error);
       });
   });
 }
 
 export function saveTsv(data: CharList, fileName: string) {
-
   // 定义 TSV 文件的路径
   const outputFilePath = path.join(__dirname, `../../output/${fileName}`);
 
   // 将 JSON 数组转换为 TSV 格式字符串
   const tsvData = [
     // headers.join('\t'), // 添加表头行
-    ...data.map(row => headers.map(header => row[header]).join('\t')) // 添加数据行
-  ].join('\n');
+    ...data
+      .filter((ele) => ele[ShiYi])
+      .map((row) => headers.map((header) => row[header]).join("\t")), // 添加数据行
+  ].join("\n");
 
   // 写入 TSV 文件
   fs.writeFile(outputFilePath, tsvData, (err) => {
     if (err) {
-      console.error('Error writing file:', err.message);
+      console.error("Error writing file:", err.message);
     } else {
-      console.log('File has been saved as', outputFilePath);
+      console.log("File has been saved as", outputFilePath);
     }
   });
 }
